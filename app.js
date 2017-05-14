ab1058525c0e152204a8b1f08d7b61ddab543be7
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-05-15 03:10:32 +0900

    v1.0.0

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 const request = require("request");
 const Twitter = require("twitter");
+const ncp = require("copy-paste");
 const fs = require("fs");
 
 const tokens = require("./tokens.json");
@@ -25,7 +26,7 @@ if(!tokens.bearerToken){
   request(options, function (error, response, body) {
     if(error) throw "token error : "+ error;
     tokens.bearerToken = body["access_token"]
-    fs.writeFile('tokens.json', JSON.stringify(tokens, null, '    '));
+      fs.writeFile('tokens.json', JSON.stringify(tokens, null, '    '));
     search();
   })
 }else{
@@ -38,23 +39,37 @@ function search(){
     consumer_secret: tokens.consumerSecret,
     bearer_token: tokens.bearerToken
   });
+  var params = {q : "参加者募集！"}   
+  setInterval(function (){
+  client.get('search/tweets', params, function(error, tweets, response) {
+    if (!error) {
+      for(var i = 0;i < tweets.statuses.length; i ++ ){
+        var text = tweets.statuses[i].text.replace(/\n/g,"");
+        if(text.match(/参加者募集！参戦ID：[0-9A-Z]{8}Lv[0-9]{2,3}\s.+https:\/\/t\.co\/[a-zA-Z0-9]+$/g)){
+          console.log("match : "+text);
 
-/*  var stream = client.stream('statuses/filter', {track: 'javascript'});
-  stream.on('data', function(event) {
-    console.log(event && event.text);
+          var _bossName = text.match(/Lv[0-9]{2,3}\s.+https:/);
+          var bossName = _bossName[0].slice(0,_bossName.length-7);
+
+          var _battleId = text.match(/参戦ID：[0-9A-Z]{8}/).slice(-8);
+          var battleId = _battleId[0].slice(-8);
+
+          console.log("bossName : "+ bossName);
+          console.log("battleId : " + battleId);
+
+          if(bossName.match(/Lv100 Dエンジェル・オリヴィエ/)){
+            console.log("copy : "+battleId);
+            (function(id){
+              return ncp.copy(id, function () {
+                console.log("copied! : "+id);
+              })
+            })(battleId);
+          }
+        }
+      }
+    }else{
+      console.log(error);
+    }
   });
-
-  stream.on('error', function(error) {
-    throw error;
-  });*/
-  
-     var params = {q: "%E6%95%91%E6%8F%B4%E8%80%85%E5%8B%9F%E9%9B%86%EF%BC%81"};
-     client.get('search/tweets', params, function(error, tweets, response) {
-     if (!error) {
-     console.log(tweets);
-     }else{
-     console.log(error)
-     }
-     });
-     
+  },1000*5);
 }
