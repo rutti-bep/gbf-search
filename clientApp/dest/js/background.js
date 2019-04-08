a773c05378a560b7ffcda60610c318f1467a80ef
rutti-bep <s.konnno.train233@gmail.com>
2019-04-08 15:58:50 +0900

    add background.js json read code #18

--- clientApp/dest/js/background.js
+++ clientApp/dest/js/background.js
@@ -15,3 +15,24 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       saveToClipboard(request.text);
     });
+
+var config = {};
+
+{
+  var file = 'dest/json/raids.json';
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', chrome.runtime.getURL(file), true);
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+      config = JSON.parse(xhr.responseText);
+      console.log(config)
+    }
+      for (key in config) {
+        if (typeof config[key] === 'string' && config[key] != '') {
+          console.log('key=' + key + ', value=' + config[key]);
+        }
+      }
+  };
+  xhr.send();
+}
+
