50b5d226d62c73b44a27b4e42941ebe97e334fd7
rutti-bep <s.konnno.train233@gmail.com>
2021-11-02 14:55:39 +0900

    wip

--- dest/js/background.js
+++ dest/js/background.js
@@ -139,29 +139,163 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 let test = ()=>{
-  var timestamp = Math.floor(new Date().getTime()/1000);;
-  const url = 'https://stream.twitter.com/1.1/statuses/filter.json?track=vim';
-  const method = 'GET'
-  var request = OAuthSimple(tokens.consumer_key,tokens.consumer_secret).sign({
-                action:method,
-                method:"HMAC-SHA1",
-                dataType:"JSON",
-                path:url,
-                parameters:'',
-                signatures:{
-                    oauth_version:'1.0',
-                    oauth_token:tokens.access_token_key,
-                    oauth_secret:tokens.access_token_secret
-                }
+  const Twitter = class {
+
+        constructor(apiKey, apiSecretKey, accessToken, accessTokenSecret) {
+            this._apiKey       = apiKey;
+            this._apiSecretKey = apiSecretKey;
+            this._accessToken       = accessToken;
+            this._accessTokenSecret = accessTokenSecret;
+        }
+
+        async get(url, params) {
+
+            const query = this._percentEncodeParams(params).map(pair => pair.key + '=' + pair.value).join('&');
+
+            const method = 'GET';
+
+            // 認証情報
+            const authorizationHeader = await this._getAuthorizationHeader(method, url, params);
+
+            const headers = {'Authorization': authorizationHeader};
+
+            // 通信
+            return fetch((! params ? url : url + '?' + query), {method, headers});
+
+        }
+
+        async _getAuthorizationHeader(method, url, params) {
+
+            // パラメータ準備
+            const oauthParams = [
+                {key: 'oauth_consumer_key'    , value: this._apiKey        },
+                {key: 'oauth_nonce'           , value: this._getNonce()    },
+                {key: 'oauth_signature_method', value: 'HMAC-SHA1'         },
+                {key: 'oauth_timestamp'       , value: this._getTimestamp()},
+                {key: 'oauth_token'           , value: this._accessToken   },
+                {key: 'oauth_version'         , value: '1.0'               }
+            ];
+
+            const allParams = this._percentEncodeParams([...oauthParams, ...params]);
+
+            this._ksort(allParams);
+
+            // シグネチャ作成
+            const signature = await this._getSignature(method, url, allParams);
+
+            // 認証情報
+            return 'OAuth ' + this._percentEncodeParams([...oauthParams, {key: 'oauth_signature', value: signature}]).map(pair => pair.key + '="' + pair.value + '"').join(', ');
+
+        }
+
+        async _getSignature(method, url, allParams) {
+
+            const allQuery = allParams.map(pair => pair.key + '=' + pair.value).join('&');
+
+            // シグネチャベース・キー文字列
+            const signatureBaseString = [
+                method.toUpperCase(),
+                this._percentEncode(url),
+                this._percentEncode(allQuery)
+            ].join('&');
+
+            const signatureKeyString = [
+                this._apiSecretKey,
+                this._accessTokenSecret
+            ].map(secret => this._percentEncode(secret)).join('&');
+
+            // シグネチャベース・キー
+            const signatureBase = this._stringToUint8Array(signatureBaseString);
+            const signatureKey  = this._stringToUint8Array(signatureKeyString);
+
+            // シグネチャ計算
+            const signatureCryptoKey = await window.crypto.subtle.importKey('raw', signatureKey, {name: 'HMAC', hash: {name: 'SHA-1'}}, true, ['sign']);
+
+            const signatureArrayBuffer = await window.crypto.subtle.sign('HMAC', signatureCryptoKey, signatureBase);
+
+            return this._arrayBufferToBase64String(signatureArrayBuffer);
+
+        }
+
+        /**
+         * RFC3986 仕様の encodeURIComponent
+         */
+        _percentEncode(str) {
+            return encodeURIComponent(str).replace(/[!'()*]/g, char => '%' + char.charCodeAt().toString(16));
+        }
+
+        _percentEncodeParams(params) {
+
+            return params.map(pair => {
+                const key   = this._percentEncode(pair.key);
+                const value = this._percentEncode(pair.value);
+                return {key, value};
             });
 
-  console.log(request);
+        }
 
-  fetch(url,{
-    cache:'no-cache',
-    headers: {
-      'authorization':request.header
-//      'authorization':'OAuth oauth_consumer_key="'+request.parameters.oauth_consumer_key+'",oauth_nonce="'+request.parameters.oauth_nonce+'",oauth_signature_method="HMAC-SHA1",oauth_timestamp="'+request.parameters.oauth_timestamp+'",oauth_token="'+request.parameters.oauth_token+'",oauth_version="1.0",oauth_signature="'+ request.signature+'"'
-    }
-  }).then((res)=>console.log(res)).catch((e)=>{console.log("e",e)})
+        _ksort(params) {
+
+            return params.sort((a, b) => {
+                const keyA = a.key;
+                const keyB = b.key;
+                if ( keyA < keyB ) return -1;
+                if ( keyA > keyB ) return 1;
+                return 0;
+            });
+
+        }
+
+        _getNonce() {
+            const array = new Uint8Array(32);
+            window.crypto.getRandomValues(array);
+            // メモ: Uint8Array のままだと String に変換できないので、Array に変換してから map
+            return [...array].map(uint => uint.toString(16).padStart(2, '0')).join('');
+        }
+
+        _getTimestamp() {
+            return Math.floor(Date.now() / 1000);
+        }
+
+        _stringToUint8Array(str) {
+            return Uint8Array.from(Array.from(str).map(char => char.charCodeAt()));
+        }
+
+        _arrayBufferToBase64String(arrayBuffer) {
+
+            const string = new Uint8Array(arrayBuffer).reduce((data, char) => {
+                data.push(String.fromCharCode(char));
+                return data;
+            }, []).join('');
+
+            return btoa(string);
+
+        }
+
+    };
+
+    const params = [
+        {key: 'track', value: 'vim'}
+    ];
+    const decoder = new TextDecoder();
+    twit = new Twitter(tokens.consumer_key,tokens.consumer_secret,tokens.access_token_key,tokens.access_token_secret);
+    twit.get("https://stream.twitter.com/1.1/statuses/filter.json",params)
+    .then((response) => response.body.getReader()) // ReadableStreamを取得する。
+    .then((reader) => {
+      function readChunk({done, value}) {
+        if(done) {
+            console.log("done");
+            return;
+        }
+        console.log(decoder.decode(value));
+
+        // 次の値を読みにいく。
+        reader.read().then(readChunk);
+      }
+      reader.read().then(readChunk);
+    
+    });
+
+
 }
+
